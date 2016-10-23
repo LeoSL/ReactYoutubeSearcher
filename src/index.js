a32501fcb3a48b2385791adb2bfd3cf2ab8e67b7
LeoSL <leosilvalima@gmail.com>
2016-10-23 21:29:19 -0200

    render the class-based component to main index js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const YOUTUBE_API_KEY = 'AIzaSyA4f2KJBDGKLdSH8g1TVwgaO9KIVf2sXfw';
+
 //   >> COMPONENT <<
 // Create a new component.
 // This component should produce to produce some HTML
 const App = () => {
-  return <div>Oi! s2</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it
