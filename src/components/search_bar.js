e2053aeba18126b09d334c63ab4dbfeec4b44313
LeoSL <leosilvalima@gmail.com>
2016-11-06 22:34:08 -0200

    youtube search actually working and add some css styling on it

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchTerm: 'First term' }; // this is the first and
+                                     // only time we change state
+                                     // like that!
+                                     // Afterwards we will use
+                                     //   this.setState({});
+  }
+
   render() {
-    return <input />;
+    return (
+      <div className="search-bar">
+        <input
+          value = {this.state.searchTerm}
+          onChange = {event => this.setState({ searchTerm: event.target.value })}
+        />
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const YOUTUBE_API_KEY = 'AIzaSyA4f2KJBDGKLdSH8g1TVwgaO9KIVf2sXfw';
 
 //   >> COMPONENT <<
 // Create a new component.
 // This component should produce to produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      }); // << same thing on ES5
+      // this.setState({ videos }); << ES6
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it
