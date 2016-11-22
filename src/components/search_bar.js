a6ff135753a278f31bae65594dc9720dc70bc44d
LeoSL <leosilvalima@gmail.com>
2016-11-22 21:43:00 -0200

    search youtube videos DONE

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,7 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { searchTerm: 'First term' }; // this is the first and
+    this.state = { searchTerm: '' }; // this is the first and
                                      // only time we change state
                                      // like that!
                                      // Afterwards we will use
@@ -16,10 +16,15 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input
           value = {this.state.searchTerm}
-          onChange = {event => this.setState({ searchTerm: event.target.value })}
+          onChange = {event => this.onInputChange(event.target.value)}
         />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({searchTerm: term});
+    this.props.onSearchTermChange(term);
   }
 }
 
